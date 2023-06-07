408d25b220f125c3df534502daffc9671c9fbfd0
Arnaud Levy <contact@arnaudlevy.com>
2023-06-07 17:48:01 +0200

    cleaner

--- wordpress-oudopo.php
+++ wordpress-oudopo.php
@@ -5,15 +5,16 @@
 * Description: Export de WordPress vers Oudopo, l'ouvroir de données potentielles.
 * Author: noesya
 * Author URI: https://www.noesya.coop
-* Version: v0.0.5
+* Version: v0.0.6
 **/
 
 if (WP_DEBUG) {
   // Local ? Probablement...
-  defined( 'OUDOPO_API' ) or define( 'OUDOPO_API', 'http://localhost:3000/api' );
+  $oudopoApi = 'http://localhost:3000/api';
 } else {
-  defined( 'OUDOPO_API' ) or define( 'OUDOPO_API', 'https://www.oudopo.org/api' );
+  $oudopoApi =  'https://www.oudopo.org/api';
 }
+defined( 'OUDOPO_API' ) or define( 'OUDOPO_API', $oudopoApi );
 
 include_once( plugin_dir_path( __FILE__ ) . 'includes/settings.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/posts.php' );
